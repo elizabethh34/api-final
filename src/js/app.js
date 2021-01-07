3934e2b58fc517ef43c20cd2598bb645e79b3aaf
Elizabeth Hellsten <elizabeth_hellsten@shaw.ca>
2021-01-07 14:37:53 -0600

    Add functions for getting trip data

--- src/js/app.js
+++ src/js/app.js
@@ -11,11 +11,22 @@ const mapboxEndpoint = 'mapbox.places/';
 const mapboxLimitParam = 'limit=10';
 const mapboxBboxParam = 'bbox=-97.325875, 49.766204, -96.953987, 49.99275';
 const mapboxAccessToken = 'access_token=pk.eyJ1IjoiZWxpemFiZXRoaDM0IiwiYSI6ImNramxpcnlpZjhnaTAyd3J4NWQ1aXBqZWUifQ.PfmZO1A6KHl_x89CFCUjjg';
+const transitBaseUrl = 'https://api.winnipegtransit.com/v3/';
+const transitEndpoint = 'trip-planner';
+const transitAPIKey = 'api-key=Ift6y-RGolzmG6rkd1op';
+const transitOriginParam = 'origin=geo/';
+const transitDestinationParam = 'destination=geo/';
 
-function getPossibleLocations(userSearch) {
+function getPossibleLocationsData(userSearch) {
   return fetch(`${mapboxBaseUrl}${mapboxEndpoint}${userSearch}.json?${mapboxBboxParam}&${mapboxLimitParam}&${mapboxAccessToken}`)
   .then(response => response.json())
   .then(data => data.features)
+}
+
+function getTripData(originLat, originLon, DestLat, DestLon) {
+  return fetch(`${transitBaseUrl}${transitEndpoint}.json?${transitAPIKey}&${transitOriginParam}${originLat},${originLon}&${transitDestinationParam}${DestLat},${DestLon}`)
+  .then(response => response.json())
+  .then(data => console.log(data))
 }
 
 function renderLocationsHTML(locationsObjArr, elemToAttachTo) {
@@ -41,7 +52,7 @@ function clearLists(element) {
 }
 
 function displayLocationInfo(inputElem, elemToAttachTo) {
-  getPossibleLocations(inputElem.value)
+  getPossibleLocationsData(inputElem.value)
   .then(locationData => {
     clearLists(elemToAttachTo);
     renderLocationsHTML(locationData, elemToAttachTo);
@@ -69,6 +80,45 @@ function checkForUserSelection() {
   }
 }
 
+function getUserOrigin() {
+  const possibleOrigins = document.querySelectorAll('.origins li');
+  let selectedOrigin;
+
+  for (const locationItem of possibleOrigins) {
+    if (locationItem.classList.contains('selected')) {
+      selectedOrigin = locationItem;
+    }
+  }
+  return selectedOrigin;
+}
+
+function getUserDestination() {
+  const possibleDestinations = document.querySelectorAll('.destinations li');
+  let selectedDestination;
+
+  for (const locationItem of possibleDestinations) {
+    if (locationItem.classList.contains('selected')) {
+      selectedDestination = locationItem;
+    }
+  }
+  return selectedDestination;
+}
+
+function callTripData() {
+  const selectedOrigin = getUserOrigin();
+  const selectedDestination = getUserDestination();
+  let originLatitude;
+  let originLongitude;
+  let destinationLatitude;
+  let destinationLongitude;
+
+  originLatitude = selectedOrigin.getAttribute('data-lat');
+  originLongitude = selectedOrigin.getAttribute('data-long');
+  destinationLatitude = selectedDestination.getAttribute('data-lat');
+  destinationLongitude = selectedDestination.getAttribute('data-long');
+  getTripData(originLatitude, originLongitude, destinationLatitude,destinationLongitude);
+}
+
 originFormElem.addEventListener('submit', event => {
   event.preventDefault();
   displayLocationInfo(originInputElem, originsListElem);
@@ -89,7 +139,8 @@ destinationsListElem.addEventListener('click', event => {
   changeSelectedClass(clickedLocation, 'destinations');
 });
 
-planTripButton.addEventListener('click', event => {
-  checkForUserSelection()
-})
+planTripButton.addEventListener('click', () => {
+  checkForUserSelection();
+  callTripData();
+});
 
